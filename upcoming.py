dca7304f6f5a5d6a5a9a4d400a841c9a670240ab
Matt Grossi <matthew.grossi@rsmas.miami.edu>
2023-12-17 15:32:08 -0500

    Initial commit

---
+++ upcoming.py
@@ -0,0 +1,38 @@
+#!/usr/local/bin/python3 
+
+import argparse
+import os
+
+# Command line arguments
+def parse_args():
+    """Parse command line arguments"""
+    parser = argparse.ArgumentParser(prog='tex2md.py',
+                usage='%(prog)s [arguments]',
+                description='Convert weekly LaTeX music list to markdown blog.')
+    parser.add_argument('-f', '--file', metavar='file', type=str,
+                        default=argparse.SUPPRESS,
+                        help='Latex file to convert')
+
+def main():
+    # Parse command line arguments
+    args = parse_args()
+
+    # Directory
+    texDir = os.path.dirname(args.file)
+    
+    # Original pdf filename (derived from tex file, if passed)
+    pdfFile = args.file.replace('tex', 'pdf')
+    
+    # New pdf filename: upcoming.pdf
+    newPdfFile = os.path.join(texDir, 'upcoming.pdf')
+    
+    # Copy new file into upcoming.pdf
+    os.system(f'cp {pdfFile} {newPdfFile}')
+    
+    # Commit to GitHub main branch for website
+    os.system(f'git add {newPdfFile}')
+    os.system('git commit -m "Update with latest list"')
+    os.system('git push origin main')
+
+if __name__ == '__main__':
+    main()
