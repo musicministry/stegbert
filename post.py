49094a2f2792bf6faa7d2e23240b109865a9f531
musicministry <oceanorganist@gmail.com>
2025-12-27 13:12:20 -0500

    Add zfill for single-digit days

--- post.py
+++ post.py
@@ -146,7 +146,7 @@ def main():
         file.write('categories:\n')
         file.write(f'  - {df.year}\n')
         file.write(f'  - {titlecase(df.season)}\n')
-        file.write(f'image: /_images/dates/{dt.datetime.strftime(date, format="%b").lower()}/{int(df["day"])}.png\n')
+        file.write(f'image: /_images/dates/{dt.datetime.strftime(date, format="%b").lower()}/{str(df["day"]).zfill(2)}.png\n')
         file.write('---\n\n')
 
         # Callout
