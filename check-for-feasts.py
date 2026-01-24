ee09fc692f79a8bef7787e052fab1d2f0ec59b45
musicministry <oceanorganist@gmail.com>
2026-01-24 15:07:27 -0500

    Include the next Sunday when checking the week to eliminate need for a second execution

--- check-for-feasts.py
+++ check-for-feasts.py
@@ -1,15 +1,14 @@
 # =============================================================================
 # author: mgrossi
-# date:   23 December 2025
+# date:   24 Jan 2025
 #
-# This script checks for any celebrations during the next week before the next
-# Sunday, since the next Sunday will be picked up by a separate GitHbub Action.
-# The argument `-y, --year` specifies which liturgical calendar to load and use
-# for checking. If any feasts do occur during the upcoming week, it will first
+# This script checks for any celebrations during the next week inclusive of the
+# next Sunday. The argument `-y, --year` specifies the liturgical calendar year
+# to use. For any feasts that occur during the upcoming week, it will first 
 # check the hymn lists for a hymn schedule. If one exists, `post.py` will be
 # executed to publish a post for it. Otherwise, a lack of music schedule
 # implies the feast or solemnity is not being celebrated (or, if it is, there
-# will be no music for it.)
+# will be no music for it), and a notification is printed for awareness.
 #
 # To execute in terminal:
 # python check-for-feasts.py --year 2026
@@ -57,35 +56,31 @@ def main():
     cal = pd.read_csv(os.path.join(process_dir, lit_calendar),
                       parse_dates=['date'], index_col='date')
 
-    # Check for other feasts this week before next Sunday
+    # Check for feasts this week including next Sunday
     today = dt.datetime.today()
     next_sun = u.next_sunday(from_date=today)
     check = (cal.index > pd.to_datetime(today)) & \
-            (cal.index < pd.to_datetime(next_sun))
+            (cal.index <= pd.to_datetime(next_sun))
 
-    if int(sum(check)) > 0:
-        # Extract any feasts this week (could be one or more)
-        ss = cal[check]
-        seasons = ss['season'].unique()
+    # Extract any feasts this week (could be one or more)
+    ss = cal[check]
+    seasons = ss['season'].unique()
 
-        # Load the hymn list(s) to see if music is scheduled
-        for season in seasons:
-            hymn_lists = u.get_hymn_lists(season)
-            hymn_lists = {k.replace('_', '-'): v for k,v in hymn_lists.items()}
+    # Load the hymn list(s) to see if music is scheduled
+    for season in seasons:
+        hymn_lists = u.get_hymn_lists(season)
+        hymn_lists = {k.replace('_', '-'): v for k,v in hymn_lists.items()}
 
-            # There may be more than one feast (e.g., Christmas Eve, Christmas Day)
-            # Check each feast for scheduled music and, if there is a scheduled
-            # line up, publish it.
-            for feast in ss['feast']:
-                if feast in hymn_lists.keys():
-                    print(f"Posting for {ss[ss['feast']==feast]['name'].values[0]}")
-                    subprocess.run(['python3', 'post.py', '--year', str(args.year),
-                                    '--publish', feast])
-                else:
-                    print(f"{ss[ss['feast']==feast]['name'].values[0]} occurs this week but no music schedule was found. Nothing to process.")
-    else:
-        print("No occasions found.")
-        return
+        # There may be more than one feast (e.g., Christmas Eve, Christmas Day)
+        # Check each feast for scheduled music and, if there is a scheduled
+        # line up, publish it.
+        for feast in ss['feast']:
+            if feast in hymn_lists.keys():
+                print(f"Posting for {ss[ss['feast']==feast]['name'].values[0]}")
+                subprocess.run(['python3', 'post.py', '--year', str(args.year),
+                                '--publish', feast])
+            else:
+                print(f"{ss[ss['feast']==feast]['name'].values[0]} occurs this week but no music schedule was found. Nothing to process.")
 
 if __name__ == "__main__":
     main()
