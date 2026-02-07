5610a9536c2d9e819498e77ab134d3e915ad27d2
musicministry <oceanorganist@gmail.com>
2026-02-07 13:49:49 -0500

    Add holy week schedules

---
+++ 2026-A/holy-week.py
@@ -0,0 +1,116 @@
+# =============================================================================
+# Lent 2026
+# Palm Sunday through Easter Vigil
+# Liturgical Year A
+#
+# Updated: February 2026
+#
+# Auto-generated hymn data dictionaries
+#
+# =============================================================================
+# Entry template:
+#
+#     seasonNN = {
+#         "Mass": "Mass setting",
+#         "parts": ["Gloria", "Holy", "Memorial Acclamation A", "Amen",
+#                   "Lamb of God"],
+#
+#         "Processional":         "NNN - Song Title",
+#
+#         "Responsorial Psalm":   "R&A p. n - YouTube video URL",
+#         "Gospel Acclamation":   "R&A p. n - YouTube video URL",
+#
+#         "Preparation of Gifts": "NNN - Song Title",
+#         "Communion":            "NNN - Song Title",
+#         "Recessional":          "NNN - Song Title",
+#     }
+#
+# Repeat this YAML block for each liturgy -- but be sure to give each block a
+# unique name that matches a `feast` name in the liturgical calendar dataframe.
+# "Mass" (str) and "parts" (list of strings) are required. Other keys can be
+# anything or as many as desired. These will be rendered in the order in which
+# they appear here. Replace N and NNN with page or hymnal song numbers.
+#
+# =============================================================================
+
+palm_sunday = {
+    "Mass": "Mass of St. Dymphna",
+    "parts": [
+        "Holy: Mass of St. Dymphna",
+        "Memorial Acclamation A: Mass of St. Dymphna",
+        "Amen: Mass of St. Dymphna",
+        "Lamb of God: Cantus Missae (B)"
+    ],
+
+    "Processional": "498 - All Glory, Laud, and Honor",
+
+    "Responsorial Psalm": "R&A p. 54 - https://youtu.be/Ch2RTsDWVaQ?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+    "Gospel Acclamation": "R&A p. 55 - https://youtu.be/aJYnvp34V1s?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+
+    "Offertory": "642 - What Wondrous Love Is This",
+    "Communion": "684 - Psalm of Hope",
+    "Recessional": "881 - Lift High the Cross"
+}
+
+holy_thursday = {
+    "Mass": "Heritage Mass",
+    "parts": [
+        "Gloria: Heritage Mass",
+        "Holy: Heritage Mass",
+        "Memorial Acclamation A: Heritage Mass",
+        "Amen: Heritage Mass",
+        "Lamb of God: Heritage Mass"
+    ],
+
+    "Processional": "839 - As We Gather at Your Table",
+
+    "Responsorial Psalm": "R&A p. 56 - https://youtu.be/0llgRx3i8s8?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+    "Gospel Acclamation": "R&A p. 57 - https://youtu.be/Bw6eU0_3h10?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+
+    "Washing-of-Feet": "696 - Ubi Cáritas (Hurd)",
+    "Offertory": "914 - Lord, Who at Your First Eucharist",
+    "Communion": "950 - Take and Eat",
+    "Transfer-of-the-Blessed-Sacrament": "509 - Pange Lingua"
+}
+
+good_friday = {
+    "Mass": "Mass of St. Dymphna",
+    "parts": [],
+
+    "Responsorial Psalm": "R&A p. 58 - https://youtu.be/sPVzFArq5bg?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+    "Gospel Acclamation": "R&A p. 59 - https://youtu.be/rrgZlKcJ8Y4?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+
+    "Veneration-of-the-Cross": "514 - Behold the Wood",
+    "Communion": "512 - O Sacred Head Surrounded",
+    "Meditation": "510 - Jesus, Remember Me"
+}
+
+easter_vigil = {
+    "Mass": "Heritage Mass",
+    "parts": [
+        "Gloria: Heritage Mass",
+        "Holy: Heritage Mass",
+        "Memorial Acclamation A: Heritage Mass",
+        "Amen: Heritage Mass",
+        "Lamb of God: Heritage Mass"
+    ],
+
+    "Psalm After First Reading": "R&A p. 60 - https://youtu.be/SjG0vq25cP8?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os ⚠️VERIFY⚠️",
+    "Psalm After Third Reading": "R&A p. 62 - https://youtu.be/VykG9JCN8D0?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os ⚠️VERIFY⚠️",
+    "Psalm After Fifth Reading": "R&A p. 64 - https://youtu.be/swXbF0Lz0mc?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os ⚠️VERIFY⚠️",
+    "Psalm After Seventh Reading": [
+        "[required] R&A p. 66 - https://youtu.be/WlYgUUMH2Iw?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os ⚠️VERIFY⚠️",
+        "[required] R&A p. 67 - https://youtu.be/NZeU5UAhhiE?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os ⚠️VERIFY⚠️"
+    ],
+    "Psalm After Epistle": "R&A p. 68 - https://youtu.be/uHORiqmMTCU?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os ⚠️VERIFY⚠️",
+
+    "Litany-of-the-Saints": "Handout - Litany of the Saints",
+    "After-Each-Baptism": "Handout - Handout",
+    "Sprinkling": "903 - Baptized in Water",
+    
+    "Offertory": "533 - Come, You Faithful, Raise the Strain",
+    "Communion": "929 - Joyous Cup",
+    "Meditation": "526 - Be Joyful, Mary",
+    "Recessional": "540 - Jesus Christ Is Risen Today"
+}
+
