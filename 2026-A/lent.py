54bf8ea01706532037b231003d1a37687fafd53a
musicministry <oceanorganist@gmail.com>
2026-02-07 13:31:30 -0500

    Create Lent schedules

---
+++ 2026-A/lent.py
@@ -0,0 +1,153 @@
+# =============================================================================
+# Lent 2026
+# Ash Wednesday through Fifth Sunday in Lent
+# Liturgical Year A
+#
+# Updated: February 2026
+#
+# Auto-generated hymn data dictionaries
+#
+# =============================================================================
+# Entry template:
+#
+#     seasonNN = {
+#         "Mass": "Mass setting",
+#         "parts": ["Gloria", "Holy", "Memorial Acclamation A", "Amen",
+#                   "Lamb of God"],
+#
+#         "Processional":         "NNN - Song Title",
+#
+#         "Responsorial Psalm":   "R&A p. n - YouTube video URL",
+#         "Gospel Acclamation":   "R&A p. n - YouTube video URL",
+#
+#         "Preparation of Gifts": "NNN - Song Title",
+#         "Communion":            "NNN - Song Title",
+#         "Recessional":          "NNN - Song Title",
+#     }
+#
+# Repeat this YAML block for each liturgy -- but be sure to give each block a
+# unique name that matches a `feast` name in the liturgical calendar dataframe.
+# "Mass" (str) and "parts" (list of strings) are required. Other keys can be
+# anything or as many as desired. These will be rendered in the order in which
+# they appear here. Replace N and NNN with page or hymnal song numbers.
+#
+# =============================================================================
+
+ash_wednesday = {
+    "Mass": "Mass of St. Dymphna",
+    "parts": [
+        "Holy: Mass of St. Dymphna",
+        "Memorial Acclamation A: Mass of St. Dymphna",
+        "Amen: Mass of St. Dymphna",
+        "Lamb of God: Cantus Missae (B)"
+    ],
+
+    "Processional": "487 - Again We Keep This Solemn Fast",
+
+    "Responsorial Psalm": "R&A p. 42 - https://youtu.be/bWlHPiSBIyo?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+    "Gospel Acclamation": "R&A p. 43 - https://youtu.be/5o0YXg4YtEQ?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+
+    "Distribution-of-Ashes": "962 - Ashes",
+
+    "Offertory": "484 - Hosea",
+    "Communion": "478 - Return to God",
+    "Recessional": "493 - Change Our Hearts"
+}
+
+lent01 = {
+    "Mass": "Mass of St. Dymphna",
+    "parts": [
+        "Holy: Mass of St. Dymphna",
+        "Memorial Acclamation A: Mass of St. Dymphna",
+        "Amen: Mass of St. Dymphna",
+        "Lamb of God: Cantus Missae (B)"
+    ],
+
+    "Processional": "479 - Lord, Who throughout These Forty Days",
+
+    "Responsorial Psalm": "R&A p. 44 - https://youtu.be/pj65fZE7510?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+    "Gospel Acclamation": "R&A p. 45 - https://youtu.be/-xKZ0v-b-vo?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+
+    "Offertory": "483 - Forty Days and Forty Nights",
+    "Communion": "910 - Shepherd of Souls",
+    "Meditation": "473 - Parce Dómine",
+    "Recessional": "964 - The Master Came to Bring Good News"
+}
+
+lent02 = {
+    "Mass": "Mass of St. Dymphna",
+    "parts": [
+        "Holy: Mass of St. Dymphna",
+        "Memorial Acclamation A: Mass of St. Dymphna",
+        "Amen: Mass of St. Dymphna",
+        "Lamb of God: Cantus Missae (B)"
+    ],
+
+    "Processional": "877 - How Good, Lord, to Be Here!",
+
+    "Responsorial Psalm": "R&A p. 46 - https://youtu.be/bEPEYxTBJsE?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+    "Gospel Acclamation": "R&A p. 47 - https://youtu.be/NDAgduh0xzg?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+
+    "Offertory": "961 - Remember Your Love",
+    "Communion": "878 - Transform Us",
+    "Meditation": "628 - You, Lord, Are Both Lamb and Shepherd",
+    "Recessional": "644 - There's a Wideness in God's Mercy"
+}
+
+lent03 = {
+    "Mass": "Mass of St. Dymphna",
+    "parts": [
+        "Holy: Mass of St. Dymphna",
+        "Memorial Acclamation A: Mass of St. Dymphna",
+        "Amen: Mass of St. Dymphna",
+        "Lamb of God: Cantus Missae (B)"
+    ],
+
+    "Processional": "481 - The Glory of These Forty Days",
+
+    "Responsorial Psalm": "R&A p. 48 - https://youtu.be/wFB0c3ca39w?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+    "Gospel Acclamation": "R&A p. 49 - https://youtu.be/UGqXKT_jvwI?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+
+    "Offertory": "724 - I Heard the Voice of Jesus Say",
+    "Communion": "941 - Eat This Bread",
+    "Recessional": "493 - Change Our Hearts"
+}
+
+lent04 = {
+    "Mass": "Mass of St. Dymphna",
+    "parts": [
+        "Holy: Mass of St. Dymphna",
+        "Memorial Acclamation A: Mass of St. Dymphna",
+        "Amen: Mass of St. Dymphna",
+        "Lamb of God: Cantus Missae (B)"
+    ],
+
+    "Processional": "593 - I Want to Walk as a Child of the Light",
+
+    "Responsorial Psalm": "R&A p. 50 - https://youtu.be/YeKe01s4loE?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+    "Gospel Acclamation": "R&A p. 51 - https://youtu.be/Lr5khW-52NY?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+
+    "Offertory": "592 - We Are the Light of the World",
+    "Communion": "684 - Psalm of Hope",
+    "Recessional": "766 - City of God"
+}
+
+lent05 = {
+    "Mass": "Mass of St. Dymphna",
+    "parts": [
+        "Holy: Mass of St. Dymphna",
+        "Memorial Acclamation A: Mass of St. Dymphna",
+        "Amen: Mass of St. Dymphna",
+        "Lamb of God: Cantus Missae (B)"
+    ],
+
+    "Processional": "479 - Lord, Who throughout These Forty Days",
+
+    "Responsorial Psalm": "R&A p. 52 - https://youtu.be/0iIu80yM4lI?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+    "Gospel Acclamation": "R&A p. 53 - https://youtu.be/6asCxIQ1ZZk?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+
+    "Offertory": "945 - I Am the Bread of Life",
+    "Communion": "684 - Psalm of Hope",
+    "Recessional": "871 - We Shall Rise Again"
+}
+
