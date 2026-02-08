16082f04212331b3b922e2de05ddb6aa3ba32cd7
musicministry <151976496+musicministry@users.noreply.github.com>
2026-02-08 09:12:25 -0500

    Confirm page numbers

--- 2026-A/holy-week.py
+++ 2026-A/holy-week.py
@@ -39,7 +39,7 @@ palm_sunday = {
         "Holy: Mass of St. Dymphna",
         "Memorial Acclamation A: Mass of St. Dymphna",
         "Amen: Mass of St. Dymphna",
-        "Lamb of God: Cantus Missae (B)"
+        "Lamb of God: Cantus Missae (version B)"
     ],
 
     "Processional": "498 - All Glory, Laud, and Honor",
@@ -95,14 +95,14 @@ easter_vigil = {
         "Lamb of God: Heritage Mass"
     ],
 
-    "Psalm After First Reading": "R&A p. 60 - https://youtu.be/SjG0vq25cP8?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os ⚠️VERIFY⚠️",
-    "Psalm After Third Reading": "R&A p. 62 - https://youtu.be/VykG9JCN8D0?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os ⚠️VERIFY⚠️",
-    "Psalm After Fifth Reading": "R&A p. 64 - https://youtu.be/swXbF0Lz0mc?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os ⚠️VERIFY⚠️",
+    "Psalm After First Reading": "R&A p. 60 - https://youtu.be/SjG0vq25cP8?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+    "Psalm After Third Reading": "R&A p. 63 - https://youtu.be/VykG9JCN8D0?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+    "Psalm After Fifth Reading": "R&A p. 65 - https://youtu.be/swXbF0Lz0mc?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
     "Psalm After Seventh Reading": [
-        "[required] R&A p. 66 - https://youtu.be/WlYgUUMH2Iw?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os ⚠️VERIFY⚠️",
-        "[required] R&A p. 67 - https://youtu.be/NZeU5UAhhiE?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os ⚠️VERIFY⚠️"
+        "[required - w/Baptisms] R&A p. 67 - https://youtu.be/WlYgUUMH2Iw?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+        "[required- no Baptisms] R&A p. 69 - https://youtu.be/yI1tWcFk02k?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os"
     ],
-    "Psalm After Epistle": "R&A p. 68 - https://youtu.be/uHORiqmMTCU?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os ⚠️VERIFY⚠️",
+    "Psalm After Epistle": "R&A p. 70 - https://youtu.be/uHORiqmMTCU?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
 
     "Litany-of-the-Saints": "Handout - Litany of the Saints",
     "After-Each-Baptism": "Handout - Handout",
