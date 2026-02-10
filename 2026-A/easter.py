aeaa5b88e541daf4293de82f3f5096624796b48c
musicministry <oceanorganist@gmail.com>
2026-02-09 23:41:11 -0500

    Create Easter schedule

---
+++ 2026-A/easter.py
@@ -0,0 +1,237 @@
+# =============================================================================
+# Easter 2026
+# First Sunday in Easter through Corpus Christi
+# Liturgical Year A
+#
+# Updated: February 2026
+#
+# Auto-generated hymn data dictionaries
+#
+# =============================================================================
+# Entry template:
+#
+#     seasonNN = {
+#         "Mass": "Mass setting",
+#         "parts": ["Gloria", "Holy", "Memorial Acclamation A", "Amen",
+#                   "Lamb of God"],
+#
+#         "Processional":         "NNN - Song Title",
+#
+#         "Responsorial Psalm":   "R&A p. n - YouTube video URL",
+#         "Gospel Acclamation":   "R&A p. n - YouTube video URL",
+#
+#         "Preparation of Gifts": "NNN - Song Title",
+#         "Communion":            "NNN - Song Title",
+#         "Recessional":          "NNN - Song Title",
+#     }
+#
+# Repeat this YAML block for each liturgy -- but be sure to give each block a
+# unique name that matches a `feast` name in the liturgical calendar dataframe.
+# "Mass" (str) and "parts" (list of strings) are required. Other keys can be
+# anything or as many as desired. These will be rendered in the order in which
+# they appear here. Replace N and NNN with page or hymnal song numbers.
+#
+# =============================================================================
+
+easter01 = {
+    "Mass": "Heritage Mass",
+    "parts": [
+        "Gloria: Heritage Mass",
+        "Holy: Heritage Mass",
+        "Memorial Acclamation A: Heritage Mass",
+        "Amen: Heritage Mass",
+        "Lamb of God: Heritage Mass"
+    ],
+
+    "Processional": "540 - Jesus Christ Is Risen Today",
+
+    "Responsorial Psalm": "R&A p. 72 - https://youtu.be/VuGLnu5nqeg?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+    "Gospel Acclamation": "R&A p. 73 - https://youtu.be/kp8WHU0pT70?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+
+    "Offertory": "520 - This Is the Feast of Victory",
+    "Communion": "526 - Be Joyful, Mary",
+    "Meditation": "Handout - Worthy is the Lamb (Manalo)",
+    "Recessional": "525 - The Strife Is O'er"
+}
+
+easter02 = {
+    "Mass": "Heritage Mass",
+    "parts": [
+        "Gloria: Heritage Mass",
+        "Holy: Heritage Mass",
+        "Memorial Acclamation A: Heritage Mass",
+        "Amen: Heritage Mass",
+        "Lamb of God: Heritage Mass"
+    ],
+
+    "Processional": "644 - There's a Wideness in God's Mercy",
+
+    "Responsorial Psalm": "R&A p. 74 - https://youtu.be/Gh359-tg9LA?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+    "Gospel Acclamation": "R&A p. 75 - https://youtu.be/OAkDSqqftzg?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+
+    "Offertory": "681 - We Remember",
+    "Communion": "532 - O Sons and Daughters",
+    "Recessional": "519 - Sing to the Mountains"
+}
+
+easter03 = {
+    "Mass": "Heritage Mass",
+    "parts": [
+        "Gloria: Heritage Mass",
+        "Holy: Heritage Mass",
+        "Memorial Acclamation A: Heritage Mass",
+        "Amen: Heritage Mass",
+        "Lamb of God: Heritage Mass"
+    ],
+
+    "Processional": "523 - Christ the Lord Is Risen Today",
+
+    "Responsorial Psalm": "R&A p. 76 - https://youtu.be/su1zUblHORU?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+    "Gospel Acclamation": "R&A p. 77 - https://youtu.be/YdaUTrnFTjs?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+
+    "Offertory": "538 - On the Journey to Emmaus",
+    "Communion": "918 - In the Breaking of the Bread",
+    "Recessional": "524 - Alleluia No. 1"
+}
+
+easter04 = {
+    "Mass": "Heritage Mass",
+    "parts": [
+        "Gloria: Heritage Mass",
+        "Holy: Heritage Mass",
+        "Memorial Acclamation A: Heritage Mass",
+        "Amen: Heritage Mass",
+        "Lamb of God: Heritage Mass"
+    ],
+
+    "Processional": "539 - Sing with All the Saints in Glory",
+
+    "Responsorial Psalm": "R&A p. 78 - https://youtu.be/oSZKN3QxY58?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+    "Gospel Acclamation": "R&A p. 79 - https://youtu.be/GfUhvUHt1Kg?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+
+    "Offertory": "712 - The King of Love My Shepherd Is",
+    "Communion": "537 - Easter Alleluia",
+    "Recessional": "525 - The Strife Is O'er"
+}
+
+easter05 = {
+    "Mass": "Heritage Mass",
+    "parts": [
+        "Gloria: Heritage Mass",
+        "Holy: Heritage Mass",
+        "Memorial Acclamation A: Heritage Mass",
+        "Amen: Heritage Mass",
+        "Lamb of God: Heritage Mass"
+    ],
+
+    "Processional": "607 - Sing a New Song (Schutte)",
+
+    "Responsorial Psalm": "R&A p. 80 - https://youtu.be/GDIJulQOsRk?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+    "Gospel Acclamation": "R&A p. 81 - https://youtu.be/GXrwzWRrNXs?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+
+    "Offertory": "458 - I Sing a Maid",
+    "Communion": "950 - Take and Eat",
+    "Recessional": "742 - The Church's One Foundation"
+}
+
+easter06 = {
+    "Mass": "Heritage Mass",
+    "parts": [
+        "Gloria: Heritage Mass",
+        "Holy: Heritage Mass",
+        "Memorial Acclamation A: Heritage Mass",
+        "Amen: Heritage Mass",
+        "Lamb of God: Heritage Mass"
+    ],
+
+    "Processional": "542 - That Easter Day with Joy Was Bright",
+
+    "Responsorial Psalm": "R&A p. 82 - https://youtu.be/SzBS0Mx56Pw?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+    "Gospel Acclamation": "R&A p. 83 - https://youtu.be/uyYpAvg2bNc?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+
+    "Offertory": "534 - Now the Green Blade Rises",
+    "Communion": "556 - Come Down, O Love Divine",
+    "Recessional": "553 - O Spirit All-Embracing"
+}
+
+ascension = {
+    "Mass": "Heritage Mass",
+    "parts": [
+        "Gloria: Heritage Mass",
+        "Holy: Heritage Mass",
+        "Memorial Acclamation A: Heritage Mass",
+        "Amen: Heritage Mass",
+        "Lamb of God: Heritage Mass"
+    ],
+
+    "Processional": "543 - Hail the Day That Sees Him Rise",
+
+    "Responsorial Psalm": "R&A p. 86 - https://youtu.be/HihEDnFVV4Y?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+    "Gospel Acclamation": "R&A p. 87 - https://youtu.be/TSsUAa_H6sA?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+
+    "Offertory": "545 - A Hymn of Glory Let Us Sing!",
+    "Communion": "681 - We Remember",
+    "Recessional": "544 - Lord, You Give the Great Commission"
+}
+
+pentecost = {
+    "Mass": "Heritage Mass",
+    "parts": [
+        "Gloria: Heritage Mass",
+        "Holy: Heritage Mass",
+        "Memorial Acclamation A: Heritage Mass",
+        "Amen: Heritage Mass",
+        "Lamb of God: Heritage Mass"
+    ],
+
+    "Processional": "559 - Come, Holy Ghost",
+
+    "Responsorial Psalm": "R&A p. 94 - https://youtu.be/Cmeo3cPnYjM?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+    "Gospel Acclamation": "R&A p. 95 - https://youtu.be/at4kPcIIz4Q?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+
+    "Offertory": "902 - O Breathe on Me, O Breath of God",
+    "Communion": "834 - We Are Many Parts",
+    "Recessional": "546 - Go to the World!"
+}
+
+holy_trinity = {
+    "Mass": "Heritage Mass",
+    "parts": [
+        "Gloria: Heritage Mass",
+        "Holy: Heritage Mass",
+        "Memorial Acclamation A: Heritage Mass",
+        "Amen: Heritage Mass",
+        "Lamb of God: Heritage Mass"
+    ],
+
+    "Processional": "567 - Holy, Holy, Holy! Lord God Almighty!",
+
+    "Responsorial Psalm": "R&A p. 96 - https://youtu.be/4CipytWuv0c?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+    "Gospel Acclamation": "R&A p. 97 - https://youtu.be/djexyE_j_D0?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+
+    "Offertory": "615 - Holy God, We Praise Thy Name",
+    "Communion": "642 - What Wondrous Love Is This",
+    "Recessional": "566 - O God, Almighty Father"
+}
+
+corpus_christi = {
+    "Mass": "Heritage Mass",
+    "parts": [
+        "Gloria: Heritage Mass",
+        "Holy: Heritage Mass",
+        "Memorial Acclamation A: Heritage Mass",
+        "Amen: Heritage Mass",
+        "Lamb of God: Heritage Mass"
+    ],
+
+    "Processional": "616 - Praise to the Lord, the Almighty",
+
+    "Responsorial Psalm": "R&A p. 98 - https://youtu.be/2lyxAUr9KUY?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+    "Gospel Acclamation": "R&A p. 99 - https://youtu.be/L0Fb8sDMJXg?list=PL1_lMtcpfrcloo19ceGJAEXPuXK2LS4os",
+
+    "Offertory": "932 - One Bread, One Body",
+    "Communion": "910 - Shepherd of Souls",
+    "Meditation": "619 - Let All Mortal Flesh Keep Silence",
+    "Recessional": "615 - Holy God, We Praise Thy Name"
+}
+
