86b96de8e64551a32800e752ee73505187c1c214
musicministry <oceanorganist@gmail.com>
2026-02-25 00:08:57 -0500

    Remove filter

--- upcoming.py
+++ upcoming.py
@@ -133,7 +133,6 @@ def main():
         file.write('      - right=0.85in\n')
         file.write('      - top=1in\n')
         file.write('      - bottom=0.85in\n')
-        file.write('filters: [formatting.lua]\n')
         file.write('---\n\n')
 
         # Preface
